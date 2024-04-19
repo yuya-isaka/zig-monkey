b53fc25b58a3c441bd23f022bed188623df43f2b
Yuya Isaka <zxwg6329@gmail.com>
2024-04-20 01:22:43 +0900

    fix build main

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,10 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var buffer_writer = std.io.bufferedWriter(std.io.getStdOut().writer());
-    const stdout = buffer_writer.writer();
-    try stdout.print("イエーいピースピース。僕はキメ顔でそういった。\n", .{});
-    try buffer_writer.flush();
-
-    std.debug.print("Ok {s}\n", .{"zig"});
+    std.debug.print("hello\n", .{});
 }
 
-test "Hello test" {
+test "test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
     defer list.deinit();
     try list.append(42);
